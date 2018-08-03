d47205ea1bc468430174c8f1f7bf94ae4aeaa79a
Ian Hollander <ianholla@gmail.com>
2018-08-03 23:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  } else if (string === shout(string)) {
+    return "YES INDEED!"
+  } else if (string === whisper(string)) {
+    return "I can't hear you!"
+  }
+}
